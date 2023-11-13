1a45d97bd8aa976bad226a57bde4377cf9306a5e
rubayeaAlketbi <sc22rhsm@leeds.ac.uk>
2023-11-13 20:20:55 +0000

    Add about page with hero section

--- CW2/app/views.py
+++ CW2/app/views.py
@@ -4,3 +4,7 @@ from flask import render_template, flash, redirect
 @app.route('/')
 def home():
     return render_template("homepage.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
