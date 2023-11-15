cfe451835abc7da22b649f8e42f140197e44dee0
rubayeaAlketbi <sc22rhsm@leeds.ac.uk>
2023-11-15 18:07:17 +0000

    Remove home route and add about route to views.py

--- CW2/app/views.py
+++ CW2/app/views.py
@@ -2,9 +2,5 @@ from app import app
 from flask import render_template, flash, redirect
 
 @app.route('/')
-def home():
-    return render_template("homepage.html")
-
-@app.route('/about')
 def about():
     return render_template("about.html")
