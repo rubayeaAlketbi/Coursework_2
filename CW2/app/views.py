8069fc4c6cc0482cafd87b11435cf8b597db8fbe
rubayeaAlketbi <sc22rhsm@leeds.ac.uk>
2023-11-16 17:59:19 +0000

    Add login form validation and admin dashboard page

--- CW2/app/views.py
+++ CW2/app/views.py
@@ -1,14 +1,27 @@
 from app import app
 from flask import render_template, flash, redirect
+from .forms import LoginForm, RegisterForm
 
 @app.route('/')
 def about():
     return render_template("about.html")
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    # Create the login form object
+    login_form = LoginForm()
+    # Validate the login form on submit
+    if(login_form.validate_on_submit()):
+        print("Form validated")
+        flash('Login requested for user {}, remember_me={}'.format(login_form.email.data, login_form.password.data))
+        print("Email: " + login_form.email.data)
+        print("Password: " + login_form.password.data)
+    return render_template("login.html",login_form = login_form)
 
 @app.route('/register')
 def register():
     return render_template("register.html")
+
+@app.route('/admin')
+def admin():
+    return render_template("admin_dash.html")
