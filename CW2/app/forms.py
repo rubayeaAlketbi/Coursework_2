e677341813911d7a15bb1dbf8a62c32dbb7264bf
rubayeaAlketbi <sc22rhsm@leeds.ac.uk>
2023-11-16 17:59:11 +0000

    Added RegisterForm to app/forms.py

--- CW2/app/forms.py
+++ CW2/app/forms.py
@@ -7,3 +7,12 @@ class LoginForm(FlaskForm):
     email = StringField('email', validators=[DataRequired()])
     password = StringField('password', validators=[DataRequired(),Length(min=8,max=20)])
     loginButton = SubmitField('Login')
+    
+''' Register form which takes in the first name, last name, email, password and confirm password of the user '''   
+class RegisterForm(FlaskForm):
+    fname = StringField('First Name', validators=[DataRequired()])
+    lname = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired(),Length(min=8,max=20)])
+    confirm = StringField('Confirm Password', validators=[DataRequired(),Length(min=8,max=20)])
+    registerButton = SubmitField('Register')
